470165736ae359fd0dd677164768659b9e1b003e
darya-k00 <dasha.kon.060203@gmail.com>
2025-03-07 23:04:47 +0300

    Update klik.py

--- klik.py
+++ klik.py
@@ -5,25 +5,21 @@ from dotenv import load_dotenv
 
 SHORTENED_LINK_DOMAINS = ['vk.cc']
 
+
 def shorten_link(token, original_url):
     api_url = 'https://api.vk.com/method/utils.getShortLink'
     params = {
         'url': original_url,
         'access_token': token,
         'v': '5.199'
-    }   
+    }
     response = requests.get(api_url, params=params)
-    response.raise_for_status() 
+    response.raise_for_status()
     response_data = response.json()
+    short_url = response_data.get('response', {}).get('short_url')
+    short_link_key = urlparse(short_url).path.split('/')[-1]
+    return short_url, short_link_key
 
-    if 'response' in response_data:
-        short_url = response_data['response']['short_url']
-        parsed_url = urlparse(short_url)
-        short_link_key = parsed_url.path.split('/')[-1]
-        return short_url, short_link_key
-    else:
-        error_message = response_data['error']['error_msg']
-        raise Exception(f"Ошибка API: {error_message}")
 
 def get_click_stats(token, short_link_key, interval='forever', intervals_count=1, extended=0):
     api_url = 'https://api.vk.com/method/utils.getLinkStats'
@@ -39,29 +35,30 @@ def get_click_stats(token, short_link_key, interval='forever', intervals_count=1
     response = requests.get(api_url, params=params)
     response.raise_for_status()
     response_data = response.json()
+    return response_data['response']['stats']
 
-    if 'response' in response_data:
-        return response_data['response']['stats']
+
+def is_shorten_link(original_url):
+    parsed_url = urlparse(original_url)
+    if parsed_url.netloc in SHORTENED_LINK_DOMAINS:
+        return True
     else:
-        error_message = response_data['error']['error_msg']
-        raise Exception(f"Ошибка API: {error_message}")
+        return False
 
-def is_shorten_link(url):
-    parsed_url = urlparse(url)
-    return parsed_url.netloc in SHORTENED_LINK_DOMAINS
+
+def main():
+    original_url = input("Введите ссылку, которую хотите сократить: ")
+
+    if is_shorten_link(original_url):
+        clicks = get_click_stats(token, shorten_link)
+        print('Количество кликов по ссылке :', clicks)
+    else:
+        shortened_link = shorten_link(token, original_url)
+        print('Сокращенная ссылка:', shortened_link)
+
 
 if __name__ == "__main__":
     load_dotenv()
-    token=os.environ.get('TOKEN')
-    original_url = input("Введите ссылку, которую хотите сократить: ")
+    token = os.environ['VK_TOKEN']
+    main()
     
-    try:
-        shortened_link = shorten_link(token, original_url)
-        print('Сокращенная ссылка:', shortened_link)
-        clicks = get_click_stats(token, shortened_link)
-        print('Количество кликов по ссылке :', clicks)
-        short_link_key = original_url.split('/')[-1]
-
-    except requests.exceptions.HTTPError as http_err:
-        print(f"HTTP error occurred: {http_err}")
-
